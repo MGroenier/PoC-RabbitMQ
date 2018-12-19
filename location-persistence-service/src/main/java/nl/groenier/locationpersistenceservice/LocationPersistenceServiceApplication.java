100935c18ccd61d6f098712b3795fffea04e8cac
Martijn Groenier <martijn.groenier@hva.nl>
2018-12-19 16:17:40 +0100

    Added location-persistence-service.

---
+++ location-persistence-service/src/main/java/nl/groenier/locationpersistenceservice/LocationPersistenceServiceApplication.java
@@ -0,0 +1,14 @@
+package nl.groenier.locationpersistenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationPersistenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationPersistenceServiceApplication.class, args);
+	}
+
+}
+
