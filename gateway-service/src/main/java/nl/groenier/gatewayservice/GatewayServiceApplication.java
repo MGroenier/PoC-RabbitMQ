229d1936eb912299a3ec536dce3d9386ad103963
Martijn Groenier <martijn.groenier@hva.nl>
2018-12-19 10:24:30 +0100

    Removed some redundant comments.

--- gateway-service/src/main/java/nl/groenier/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/nl/groenier/gatewayservice/GatewayServiceApplication.java
@@ -11,27 +11,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class GatewayServiceApplication {
 
-//	@Bean
-//	Binding binding(Queue queue, DirectExchange exchange) {
-//		return BindingBuilder.bind(queue).to(exchange).with("queue-b");
-//	}
-//
-//	@Bean
-//	Queue queue2() {
-//		return new Queue("queue-a", false);
-//	}
-//
-//	@Bean
-//	DirectExchange exchange2() {
-//		return new DirectExchange("spring-boot-direct-exchange");
-//	}
-//
-//	@Bean
-//	Binding binding2(Queue queue, DirectExchange exchange) {
-//		return BindingBuilder.bind(queue).to(exchange).with("queue-a");
-//	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+
 }
